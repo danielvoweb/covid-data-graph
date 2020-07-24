481cc6a9be38211a923b9e2a2212a85a6fd0a6df
Daniel Vo <dan@daniel-vo.com>
2020-07-23 22:50:57 -0500

    Refactored business logic by pulling const for USA

--- CovidDataGraph/BusinessLogic.cs
+++ CovidDataGraph/BusinessLogic.cs
@@ -6,11 +6,12 @@ namespace CovidDataGraph
 {
     public static class BusinessLogic
     {
+        private const string USA = "USA";
         public static IEnumerable<CountryViewModel> GetTopByPopDensity(this IDictionary<string, Country> countries)
         {
             var countryViewModels = countries.Select(x => new CountryViewModel(x.Value.Location, x.Value.Population_Density));
             var stdDev = countryViewModels.Select(x => x.PopulationDensity).StdDev();
-            var us_PopulationDensity = countries.First(x => x.Key == "USA").Value.Population_Density;
+            var us_PopulationDensity = countries.First(x => x.Key == USA).Value.Population_Density;
 
             return countryViewModels
                 .Where(x => x.PopulationDensity <= us_PopulationDensity + stdDev)
